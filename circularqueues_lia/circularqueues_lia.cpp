0d50ca558be223280118537c4ab6a0f85535058f
syawaliaaaz <helfinasyawaliaaa@gmail.com>
2024-05-30 11:07:23 +0700

    Deklarasi void remove

--- circularqueues_lia/circularqueues_lia.cpp
+++ circularqueues_lia/circularqueues_lia.cpp
@@ -20,31 +20,70 @@ public:
         cout << "Enter a number: ";
         cin >> num;
         cout << endl;
-    }
 
+        //cek apakah antrian penuh
+        if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1)) {
+            cout << "\nQueue overflow\n";
+            return;
+        }
 
-
-    //cek apakah antrian penuh
-    if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1)) {
-        cout << "\nQueue overflow\n";
-        return;
-    }
-
-    //cek apakah antrian kosong
-    if (FRONT == -1) {
-        FRONT = 0;
-        REAR = 0;
-    }
-
-    else {
-        // Jika REAR berada di posisi terakhir array, kembali ke awal array
-        if (REAR == max - 1)
+        //cek apakah antrian kosong
+        if (FRONT == -1) {
+            FRONT = 0;
             REAR = 0;
-        else
-            REAR = REAR + 1;
+        }
+
+        else {
+            // Jika REAR berada di posisi terakhir array, kembali ke awal array
+            if (REAR == max - 1)
+                REAR = 0;
+            else
+                REAR = REAR + 1;
+        }
+        queue_array[REAR] = num;
     }
-    queue_array[REAR] = num;
-}
+    void remove() {
+        //cek apakah antrian kosong
+        if (FRONT == -1) {
+            cout << "Queue underflow\n";
+            return;
+        }
+        cout << "\nThe element deleted from the queue is: " << queue_array[FRONT] << "\n";
+
+        //cek jika antrian hanya memiliki satu elemen
+        if (FRONT == REAR) {
+            FRONT = -1;
+            REAR = -1;
+        }
+        else {
+            //jika elemen yang dihapus berada di posisi terakhir array, kembali ke awal array
+            if (FRONT == max - 1)
+                FRONT = 0;
+            else
+                FRONT = FRONT + 1;
+        }
+    }
+
+    void display() {
+
+        // cek apakah antrian kosong
+        if (FRONT == -1) {
+            cout << "Queue is empty\n";
+            return;
+        }
+
+        cout << "\nElements in the queue are...\n";
+
+        // Jika FRONT <= REAR, iterasi dari FRONT hingga REAR
+        if (FRONT <= REAR) {
+            while (FRONT <= REAR) {
+
+            }
+        }
+    }
+};
+
+
 
 
 
