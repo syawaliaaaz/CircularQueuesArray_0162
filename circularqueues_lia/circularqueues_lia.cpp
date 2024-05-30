644fd94fee21e09bae8dcc322548335ca63c2984
syawaliaaaz <helfinasyawaliaaa@gmail.com>
2024-05-30 11:31:50 +0700

    Deklarasi Integer Main dengan looping while ( true) beserta cout dan cin

--- circularqueues_lia/circularqueues_lia.cpp
+++ circularqueues_lia/circularqueues_lia.cpp
@@ -117,3 +117,25 @@ int main() {
             cout << "Enter your choice (1-4): ";
             cin >> ch;
             cout << endl;
+
+            switch (ch) {
+            case '1': {
+
+                q.insert();
+                break;
+            }
+            case '2': {
+                q.remove();
+                break;
+            }
+            case '3': {
+                q.display();
+                break;
+            }
+            case '4': {
+                return 0;
+            }
+            default: {
+                cout << "Invalid option!!" << endl;
+                break;
+            }
