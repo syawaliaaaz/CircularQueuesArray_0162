eeb69b70624787f5ed66066dca61464f2386c195
syawaliaaaz <helfinasyawaliaaa@gmail.com>
2024-05-30 10:27:57 +0700

    Add project files.

---
+++ circularqueues_lia/circularqueues_lia.cpp
@@ -0,0 +1,19 @@
+// circularqueues_lia.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+class Queues {
+    int FRONT, REAR, max = 5;
+    int queue_array[5];
+};
+
+public:
+    Queues
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
