cc1831139c32bf95b48c8aab04b576fb0cafce2b
syawaliaaaz <helfinasyawaliaaa@gmail.com>
2024-05-30 11:15:11 +0700

    Cek antrian kosong di void display

--- circularqueues_lia/circularqueues_lia.cpp
+++ circularqueues_lia/circularqueues_lia.cpp
@@ -77,7 +77,25 @@ public:
         // Jika FRONT <= REAR, iterasi dari FRONT hingga REAR
         if (FRONT <= REAR) {
             while (FRONT <= REAR) {
+                cout << queue_array[FRONT] << " ";
+                FRONT++;
 
+            }
+            cout << endl;
+        }
+        else {
+            // Jika FRONT > REAR, iterasi dari FRONT hingga akhir array
+            while (FRONT <= max - 1) {
+                cout << queue_array[FRONT] << " ";
+                FRONT++;
+            }
+
+            FRONT = 0;
+
+            // Iterasi dari awal array hingga REAR
+            while (FRONT <= REAR) {
+                cout << queue_array[FRONT] << " ";
+                FRONT++;
             }
         }
     }
