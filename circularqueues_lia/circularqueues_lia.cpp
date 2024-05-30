1f64c70981b9ed98da5dd52dfd4a8f151a8acc08
syawaliaaaz <helfinasyawaliaaa@gmail.com>
2024-05-30 10:41:44 +0700

    Cek antrian dengan conditional statement if

--- circularqueues_lia/circularqueues_lia.cpp
+++ circularqueues_lia/circularqueues_lia.cpp
@@ -7,7 +7,7 @@ using namespace std;
 class Queues {
     int FRONT, REAR, max = 5;
     int queue_array[5];
-};
+
 
 public:
     Queues() {
@@ -22,7 +22,29 @@ public:
         cout << endl;
     }
 
-    
+
+
+    //cek apakah antrian penuh
+    if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1)) {
+        cout << "\nQueue overflow\n";
+        return;
+    }
+
+    //cek apakah antrian kosong
+    if (FRONT == -1) {
+        FRONT = 0;
+        REAR = 0;
+    }
+
+    else {
+        // Jika REAR berada di posisi terakhir array, kembali ke awal array
+        if (REAR == max - 1)
+            REAR = 0;
+        else
+            REAR = REAR + 1;
+    }
+    queue_array[REAR] = num;
+}
 
 
 int main()
